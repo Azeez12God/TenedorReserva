47c6e39f09333b0c46a35e0ea1808ae2bb0a768e
Alberto <albertolopez.alu@iespacomolla.es>
2024-12-02 20:44:42 +0100

    Empezando la vista de crear reserva

--- src/App/Controller/ReservasController.php
+++ src/App/Controller/ReservasController.php
@@ -7,25 +7,25 @@ use App\Controller\InterfaceController;
 class ReservasController implements InterfaceController
 {
     //GET /bookings
-    public function index(){
+    public function index($api){
         //include_once "../View/Users/indexUser.php";
     }
 
     //GET /bookings/create
-    public function create(){
+    public function create($api){
         //Aquí mostraríamos el formulario de registro
         echo "Formulario de registro de una reserva";
 
     }
 
     //POST /bookings
-    public function store(){
+    public function store($api){
         //Guardaría en la base de datos el usuario
         echo "Función para guardar una reserva";
     }
 
     //GET /bookings/{id_reserva}/edit
-    public function edit($id){
+    public function edit($id, $api){
         //Mostraría un formulario con los datos del usuario
         echo "Formulario para editar los datos de la reserva $id";
 
@@ -33,7 +33,7 @@ class ReservasController implements InterfaceController
 
 
     //PUT /bookings/{id_reserva}
-    public function update($id){
+    public function update($id, $api){
         //Guardaría los datos modificados del usuario
         echo "Función para actualizar los datos en la BD de la reserva $id";
 
@@ -41,13 +41,13 @@ class ReservasController implements InterfaceController
 
 
     //GET /bookings/{id_reserva}
-    public function show($id){
+    public function show($id, $api){
         echo "Mostar los datos de la reserva $id";
     }
 
 
     //DELETE /bookings/{id_reserva}
-    public function destroy($id){
+    public function destroy($id, $api){
         echo "Función para borrar los datos de la reserva $id";
     }
 }
