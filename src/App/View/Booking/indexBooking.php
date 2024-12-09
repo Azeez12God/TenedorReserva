dabff63fc4a36df06d0637e9774172cd8f01df5a
Adrian Pareja <adrianpareja.alu@iespacomolla.es>
2024-12-09 19:14:05 +0100

    edit medio hecho en el Controller, errorBooking hecho e indexBooking hecho

---
+++ src/App/View/Booking/indexBooking.php
@@ -0,0 +1,18 @@
+<?php
+$titulo="ReserDAWtions";
+include_once "./environment.php";
+include_once "View/template/inicio.php";
+include_once "View/template/arribaNavegacion.php";
+include_once "View/template/navegacion.php";
+?>
+
+    <section id="services" class="text-center">
+        <?="Aquí aparecerán un listado de las reservas"?>
+    </section>
+
+<?php
+include_once "View/template/footer.php";
+include_once "View/template/modal.php";
+
+include_once "View/template/final.php";
+
